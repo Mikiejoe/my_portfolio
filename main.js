c4aceda648da28d74ff95601a126119148178997
2002joe <omoshojoe02@gmail.com>
2023-02-04 21:43:51 +0300

    functionalised the next button and the timestamp

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist' );
 const music = document.querySelector("audio");
 const prevbtn = document.getElementById("prev");
+const totalTime = document.getElementById("total");
+const elapsedTime = document.getElementById("elapsed");
 const playbtn = document.getElementById("play");
 const nextbtn = document.getElementById("next");
+const progressContainer = document.getElementById("progress");
+const progress = document.getElementById("progress-bar");
 
 const songs = [
   {
@@ -23,7 +27,7 @@ const songs = [
     artist: 'juice wrld ft future', 
   }, 
   {
-    name: 'seious.mp3', 
+    name: 'serious', 
     displayName: 'Serious', 
     artist: 'Nyashinski', 
   }, 
@@ -54,28 +58,59 @@ function pauseSong(){
 }
 
 playbtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
-var index = 4;
+var index = 0;
 function changeSong(song){
   title.textContent = song.displayName;
   artist.textContent = song.artist;
   music.src = `/music/${song.name}.mp3`;
   image.src = `/images/${song.name}.jpg`;
+  
 }
 
 function nextSong() {
   index +=1;
-  changeSong(index);
+  if(index<songs.length){
+  
+  } else{
+    index=0;
+  } 
+  changeSong(songs[index]);
   playSong();
-  console.log(songs[index]);
 }
 function prevSong() {
   
   index--;
-  changeSong(index);
+  if(index<0){
+    index=(songs.length-1)
+  }
+  changeSong(songs[index]);
   playSong();
-  console.log(index);
+  
 } 
+
+function updateTime(e){
+  if(isPlaying){
+    const {duration, currentTime} = e.srcElement;
+    const percent = (currentTime/duration)*100;
+    progress.style.width = `${percent}%`;
+    const totalMinutes = Math.floor(duration/60);
+    let totalSeconds = Math.floor(duration%60);
+    if(totalSeconds<10){
+      totalSeconds = `0${totalSeconds}`;
+    }
+    if(totalMinutes){
+    totalTime.textContent = `${totalMinutes}:${totalSeconds}`;} 
+    const elapsedMinutes = Math.floor(currentTime/60);
+    let elapsedSeconds = Math.floor(currentTime%60);
+    if(elapsedSeconds<10){
+      elapsedSeconds= `0${elapsedSeconds}`;
+    }
+    elapsedTime.textContent = `${elapsedMinutes}:${elapsedSeconds}`;
+  }
+}
+
 changeSong(songs[index]);
 prevbtn.addEventListener('click', prevSong);
 nextbtn.addEventListener('click', nextSong);
-console.log(index)
+music.addEventListener('timeupdate', updateTime);
+
