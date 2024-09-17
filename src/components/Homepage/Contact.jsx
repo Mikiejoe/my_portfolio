90184cff51b540a8a5ef895c133e770675847deb
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-09-17 16:16:50 +0300

    add send message functionality

--- src/components/Homepage/Contact.jsx
+++ src/components/Homepage/Contact.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs";
 import { MdMail, MdPhone, MdWhatsapp } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 function Contact() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [response, setResponse] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubjectChange = (e) => {
+    setSubject(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email && subject && message) {
+      // Send the form data to the Django backend
+      const formData = {
+        email,
+        subject,
+        message,
+        name,
+      };
+
+      try {
+        const response = await fetch("https://portfolio-backend-tesg.onrender.com/api/send-email/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          // Handle the response from the backend
+          setSuccess(true);
+          setResponse(data.message);
+          setTimeout(() => {
+            setSuccess(false);
+            setResponse("");
+          }, 5000);
+          console.log(data);
+        } else {
+          const data = await response.json();
+          console.log(data);
+          setError(true);
+          setResponse(data.message);
+          setTimeout(() => {
+            setError(false);
+            setResponse("");
+          }, 5000);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        setEmail("");
+        setSubject("");
+        setMessage("");
+        setName("");
+      }
+    }
+  };
+
   return (
     <div
       id="contact"
@@ -17,31 +92,55 @@ function Contact() {
       <div className="w-full flex flex-col md:flex-row space-x-4 mt-2">
         <div className="w-full text-start p-2">
           <h1 className="text-xl">Message Me</h1>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="w-full mt-4 h-10 rounded-sm overflow-hidden">
               <input
                 type="text"
-                className="w-full h-full p-2"
+                className="w-full h-full text-black p-2"
+                placeholder="Full Name"
+                value={name}
+                required
+                onChange={handleNameChange}
+              />
+            </div>
+            <div className="w-full mt-4 h-10 rounded-sm overflow-hidden">
+              <input
+                type="email"
+                className="w-full h-full text-black p-2"
                 placeholder="Email"
+                required
+                value={email}
+                onChange={handleEmailChange}
               />
             </div>
             <div className="w-full mt-4 h-10 rounded-sm overflow-hidden">
               <input
                 type="text"
-                className="w-full h-full p-2"
+                className="w-full h-full text-black p-2"
                 placeholder="Your message subject"
+                value={subject}
+                required
+                onChange={handleSubjectChange}
               />
             </div>
             <div className="w-full mt-4 rounded-sm overflow-hidden">
               <textarea
-                className="w-full h-full p-2"
+                className="w-full h-full p-2 text-black"
                 rows={6}
                 placeholder="Your Message"
+                required
+                value={message}
+                onChange={handleMessageChange}
               />
             </div>
+
+            {success && <div className="mt-4 text-white bg-green-500 p-2 text-center rounded-md">{response}</div>}
+            {error && <div className="mt-4 text-white bg-red-500 p-2 text-center rounded-md">{response}</div>}
+
             <div className="mt-4 rounded-md overflow-hidden">
               <input
                 type="submit"
+                onSubmit={handleSubmit}
                 className="w-full h-full bg-secondary p-2"
                 value={"Contact me"}
               />
@@ -50,7 +149,9 @@ function Contact() {
         </div>
         <div className="w-full  text-start p-2">
           <div className="w-full text-start space-y-2 p-2">
-            <h1 className="text-xl sm:text-center font-semibold mb-2">Contact Information</h1>
+            <h1 className="text-xl sm:text-center font-semibold mb-2">
+              Contact Information
+            </h1>
             <div className="sm:flex md:block justify-between">
               <div className="flex flex-col justify-between mt-6 space-y-4">
                 <div className="flex space-x-3">
